3c62db1ce4e5bbb9d9254e4f50b00d2c77cf143f
tyujrtuy <wosilechang@gmail.com>
2026-08-13 01:31:14 +0800

    Sync v2.1.54 zhCN localization

--- LariasWeeklyChecklist/CHANGELOG.md
+++ LariasWeeklyChecklist/CHANGELOG.md
@@ -1,7 +1,7 @@
 # Larias' Weekly Checklist
 
-## [v2.1.53](https://github.com/Devbezos/Larias-Weekly-Checklist/tree/v2.1.53) (2026-08-12)
-[Full Changelog](https://github.com/Devbezos/Larias-Weekly-Checklist/compare/v2.1.52...v2.1.53) 
+## [v2.1.54](https://github.com/Devbezos/Larias-Weekly-Checklist/tree/v2.1.54) (2026-08-12)
+[Full Changelog](https://github.com/Devbezos/Larias-Weekly-Checklist/compare/v2.1.53...v2.1.54) 
 
-- Release v2.1.53  
-- Post-release: bump version to 2.1.53  
+- Release v2.1.54  
+- Post-release: bump version to 2.1.54  
--- LariasWeeklyChecklist/Locales/enUS_Data.lua
+++ LariasWeeklyChecklist/Locales/enUS_Data.lua
@@ -5,7 +5,7 @@ NOTE: This is the canonical enUS dataset; other locales must keep IDs identical
 so completion tracking stays consistent across locales.
 ]]
 
--- @sheet-version: 6
+-- @sheet-version: 7
 
 local LOCALE = "enUS"
 
@@ -17,7 +17,7 @@ if type(reg) ~= "table" then
     _G[LOCALE_REGISTRY_KEY] = reg
 end
 if type(reg.data) ~= "table" then reg.data = {} end
-reg.sheet_version = "6"
+reg.sheet_version = "7"
 
 local DATASET = {
 
@@ -30,8 +30,9 @@ local DATASET = {
             { id = "e2863827", text = "Complete 2x Hard Prey for 279 ilvl gear if needed" },
             { id = "66e39766", text = "Complete the new Lair raid boss on story mode difficulty for fun and LFR gear." },
             { id = "70348198", text = "Complete weekly spark quest" },
+            { id = "3e1089cd", text = "Complete \"Sparks of War\" weekly quest for 2nd spark - you can easily do this with Voidstorm world quests on war mode" },
             { id = "690c60ea", text = "Unlock a Season 2 Crucible of Erratic Energies that can be upgraded to 295 by picking up a quest at the voidstorm upgrade altar. Sim it!" },
-            { id = "be465df9", text = "Complete Vaults of Atal'Utek: A Toxic Tour for 1/2 of a 1/6 champion item and more importantly a Season 2 delve map. DO NOT USE THE MAP YET!" },
+            { id = "3bf6be20", text = "Complete Vaults of Atal'Utek: A Toxic Tour for 1/2 of a 1/6 champion item and more importantly a Season 2 delve map. DO NOT USE THE MAP YET! (read the next line, you'll use it this week)" },
             { id = "e87e921a", text = "Push delves until you unlock t11. While doing this, once you've reached a t8 delve, do the t8 delve with the map buff to get a 1/6 hero item. Should be the only source of hero items this week." },
             { id = "090e43c1", text = "If you are not doing raid splits next week, you should fill out your delve vault to try to get tier pieces. If you are doing splits next week, just make sure that you have 3 total slots filled so you can get a socket." },
         },
